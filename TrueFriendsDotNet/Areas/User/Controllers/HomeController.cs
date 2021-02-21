e5e76f0f1dd0285d1df3479602f2ceed4d4141ab
Paul <webdevpaulmason@gmail.com>
2021-02-21 14:01:57 -0800

    Add User and Admin Areas

--- TrueFriendsDotNet/Areas/User/Controllers/HomeController.cs
+++ TrueFriendsDotNet/Areas/User/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using TrueFriendsDotNet.Models.ViewModels;
 
-namespace TrueFriendsDotNet.Controllers
+namespace TrueFriendsDotNet.Areas.User.Controllers
 {
+    [Area("User")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
